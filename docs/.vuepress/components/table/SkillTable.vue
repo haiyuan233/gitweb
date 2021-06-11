9ca9a69d6c5cdae70a3bbe0c1e1fd7965d570028
haiyuan <2718316290@qq.com>
2021-06-11 08:33:46 +0800

    op

---
+++ docs/.vuepress/components/table/SkillTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <h2>
+      <img v-if="data.icon !== null" :src="data.icon" style="vertical-align:middle" width="30">
+      <el-avatar  shape="square" :size="30" v-if="data.icon === null" style="vertical-align:middle"> 暂无 </el-avatar>
+      {{data.name}}
+    </h2>
+    <div style="margin-bottom: 20px">
+      <template>
+        <template v-if="data.type"><el-tag effect="dark">主动技能</el-tag></template>
+        <template v-else><el-tag type="info" effect="dark">被动技能</el-tag></template>
+      </template>
+      <template>
+        <template v-if="data.restrict" ><el-tag type="danger" effect="dark">主职专用</el-tag></template>
+        <template v-else><el-tag type="success" effect="dark">副职可用</el-tag></template>
+      </template>
+      <template>
+        <template v-if="data.restrictWp !== null" ><el-tag type="danger" effect="dark">{{data.restrictWp}}限定</el-tag></template>
+      </template>
+    </div>
+
+    <li>{{data.message}}</li>
+
+    <div style="padding-top: 50px" v-if="data.level !== null">
+      <el-table
+          :data="data.level">
+        <el-table-column
+            prop="name"
+            label="设定项目"
+            width="200"
+            align="center"
+            >
+        </el-table-column>
+        <template v-for="m in data.maxlevel">
+          <el-table-column
+              :prop="data[m-1]"
+              :label="'Lv' + m"
+              width="60"
+              align="center"
+              >
+            <template slot-scope="scope">
+              {{scope.row.data[m-1] + scope.row.unit}}
+            </template>
+          </el-table-column>
+        </template>
+
+      </el-table>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SkillTable",
+  props: {
+    data: Object
+  },
+  data: function() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+table {
+  margin: 0 0 !important;
+}
+</style>
